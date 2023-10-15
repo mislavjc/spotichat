27afe684dafe027fc4d9f6fed2708e7dc01dd30b
Mislav <mislavjc@gmail.com>
2023-10-15 14:59:27 +0200

    feat: add basic ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,5 @@
-import { LoginButton, LogoutButton } from 'components/buttonts';
 import { Chat } from 'components/chat';
 
-import { getSpotifyClient } from 'lib/spotify';
-
 export default async function Home() {
-  const spotify = await getSpotifyClient();
-
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        {spotify?.token ? <LogoutButton /> : <LoginButton />}
-
-        <h1>Server Session</h1>
-        <Chat />
-      </div>
-    </main>
-  );
+  return <Chat />;
 }
--- components/chat.tsx
+++ components/chat.tsx
@@ -1,34 +1,75 @@
 'use client';
 
 import { useChat } from 'ai/react';
+import { CornerDownLeft } from 'lucide-react';
+import remarkGfm from 'remark-gfm';
+import remarkMath from 'remark-math';
+
+import { useEnterSubmit } from 'hooks/use-enter-submit';
+
+import { Button } from './ui/button';
+import { Textarea } from './ui/textarea';
+import { MemoizedReactMarkdown } from './markdown';
 
 export const Chat = () => {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
 
+  const { formRef, onKeyDown } = useEnterSubmit();
+
   return (
-    <main className="mx-auto flex h-screen w-full max-w-lg flex-col p-24">
-      <section className="mb-auto">
-        {messages.map((m) => (
-          <div className="mb-4" key={m.id}>
-            {m.role === 'user' ? 'User: ' : 'AI: '}
-            {m.content}
-          </div>
-        ))}
-      </section>
-      <form className="flex space-x-4" onSubmit={handleSubmit}>
-        <input
-          className="rounded-md p-2 text-black"
-          value={input}
-          onChange={handleInputChange}
-          placeholder="Say something..."
-        />
-        <button
-          className="rounded-md border-2 border-solid border-white p-2"
-          type="submit"
+    <main className="relative mx-auto flex min-h-screen w-full max-w-screen-md flex-col p-8">
+      <div>
+        <div className="mb-20 flex flex-col gap-8">
+          {messages.map((m) => (
+            <div
+              key={m.id}
+              className={`flex gap-4 ${
+                m.role === 'user' ? 'justify-end' : 'justify-start'
+              }`}
+            >
+              <div
+                className={`${
+                  m.role === 'user' ? 'bg-green-500 text-white' : 'bg-gray-100'
+                } rounded-md px-4 py-2`}
+              >
+                <MemoizedReactMarkdown
+                  remarkPlugins={[remarkGfm, remarkMath]}
+                  components={{
+                    p({ children }) {
+                      return <p className="mb-2 last:mb-0">{children}</p>;
+                    },
+                  }}
+                >
+                  {m.content}
+                </MemoizedReactMarkdown>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <form
+          onSubmit={handleSubmit}
+          className="fixed inset-x-0 bottom-0 mx-auto w-full max-w-screen-md"
+          ref={formRef}
         >
-          Send
-        </button>
-      </form>
+          <div className="relative rounded-t-lg border bg-white p-4">
+            <Textarea
+              value={input}
+              onChange={handleInputChange}
+              className="min-h-[50px] w-full resize-none bg-transparent px-4 py-[1rem] pr-20 focus-within:outline-none sm:text-sm"
+              placeholder="Type your question here..."
+              onKeyDown={onKeyDown}
+            />
+            <Button
+              type="submit"
+              className="absolute right-4 top-6 mr-4 mt-2"
+              disabled={!input}
+            >
+              <CornerDownLeft />
+            </Button>
+          </div>
+        </form>
+      </div>
     </main>
   );
 };
